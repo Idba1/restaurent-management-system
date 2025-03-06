1e64e7d4351a82913ce7b3f9c16d448ad4bfaa8a
Idba1 <moniraislamself@gmail.com>
2025-03-06 15:47:23 +0600

    add popular menu

---
+++ client/src/Pages/Home/PopularMenu.jsx
@@ -0,0 +1,18 @@
+import SectionTitle from "../../Components/SectionTitle";
+
+const PopularMenu = () => {
+
+    return (
+        <>
+            <section className="mb-12">
+                <SectionTitle
+                    heading="From Our Menu"
+                    subHeading="Popular Items"
+                ></SectionTitle>
+            </section>
+        </>
+
+    );
+};
+
+export default PopularMenu;
