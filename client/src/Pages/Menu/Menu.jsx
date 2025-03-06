35404ea2e451d5ce45051437aa76a171accc3975
Idba1 <moniraislamself@gmail.com>
2025-03-07 01:24:08 +0600

    display different menu category items with cove

--- client/src/Pages/Menu/Menu.jsx
+++ client/src/Pages/Menu/Menu.jsx
@@ -1,13 +1,23 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover';
-import menuImg from "../../assets/menu/banner3.jpg";
-import PopularMenu from '../Home/PopularMenu';
-import MenuItem from '../Shared/MenuItem';
+import soupImg from '../../assets/menu/soup-bg.jpg';
+import menuImg from '../../assets/menu/banner3.jpg';
+import saladImg from '../../assets/menu/salad-bg.jpg';
+import pizzaImg from '../../assets/menu/pizza-bg.jpg';
+import dessertImg from '../../assets/menu/dessert-bg.jpeg';
 import SectionTitle from '../../Components/SectionTitle';
+import useMenu from '../../hooks/useMenu';
+import MenuCategory from './MenuCategory';
 
 
 const Menu = () => {
 
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
 
     return (
         <div>
@@ -16,6 +26,12 @@ const Menu = () => {
             </Helmet>
             <Cover img={menuImg} title={"OUR MENU"} ></Cover>
             <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items  */}
+            <MenuCategory items={desserts} title="dessert" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
         </div>
     );
 };
--- client/src/Pages/Menu/MenuCategory.jsx
+++ client/src/Pages/Menu/MenuCategory.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom';
-import Cover from '../Shared/Cover';
-import MenuItem from '../Shared/MenuItem';
+import { Link } from "react-router-dom";
+import Cover from "../Shared/Cover";
+import MenuItem from "../Shared/MenuItem";
 
-const MenuCategory = ({ items, title, img }) => {
+const MenuCategory = ({items, title, img}) => {
     return (
         <div className='pt-8'>
-            {title && <Cover img={img} title={title}></Cover>}
+            { title && <Cover img={img} title={title}></Cover>}
             <div className="grid md:grid-cols-2 gap-10 my-16">
                 {
                     items.map(item => <MenuItem
@@ -15,7 +15,7 @@ const MenuCategory = ({ items, title, img }) => {
                 }
             </div>
             <Link to={`/order/${title}`}>
-                <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+            <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
             </Link>
         </div>
     );
