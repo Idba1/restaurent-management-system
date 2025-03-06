2c1f7f3199da0be6db08b1109ab540f4d6fd8889
Idba1 <moniraislamself@gmail.com>
2025-03-07 00:37:51 +0600

    create custom hook useMenu to load menu data

--- client/src/Pages/Menu/Menu.jsx
+++ client/src/Pages/Menu/Menu.jsx
@@ -2,9 +2,12 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover';
 import menuImg from "../../assets/menu/banner3.jpg";
 import PopularMenu from '../Home/PopularMenu';
+import MenuItem from '../Shared/MenuItem';
+import SectionTitle from '../../Components/SectionTitle';
 
 
 const Menu = () => {
+
 
     return (
         <div>
@@ -12,9 +15,7 @@ const Menu = () => {
                 <title>Debug And Dine | Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"OUR MENU"} ></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title={"OUR MENU"} ></Cover>
-            <PopularMenu></PopularMenu>
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
         </div>
     );
 };
