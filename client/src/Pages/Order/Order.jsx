aaa70f3dba83bef4e21dbdbae20500ca0d2afd51
Idba1 <moniraislamself@gmail.com>
2025-03-07 13:28:32 +0600

    add react tabs

--- client/src/Pages/Order/Order.jsx
+++ client/src/Pages/Order/Order.jsx
@@ -1,16 +1,33 @@
 import { Helmet } from 'react-helmet-async';
-import orderCoverImg from '../../assets/home/slide1.jpg'
+import { useState } from 'react';
+import orderCoverImg from '../../assets/dashboard/image-5.jpg';
 import Cover from '../Shared/Cover';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Order = () => {
-
+    const [tabIndex, setTabIndex] = useState(0); 
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Order Food</title>
             </Helmet>
-            <Cover img={orderCoverImg} title="Order Food"></Cover>
+            <Cover img={orderCoverImg} title="Order Food" />
+
+            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                <TabList>
+                    <Tab>Salad</Tab>
+                    <Tab>Pizza</Tab>
+                    <Tab>Soup</Tab>
+                    <Tab>Dessert</Tab>
+                    <Tab>Drinks</Tab>
+                </TabList>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+                <TabPanel></TabPanel>
+            </Tabs>
         </div>
     );
 };
