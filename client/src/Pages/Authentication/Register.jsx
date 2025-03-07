5cb90f693de6a9a055df6eb0f8bb1f44c952b73c
Idba1 <moniraislamself@gmail.com>
2025-03-07 18:40:19 +0600

    update register

--- client/src/Pages/Authentication/Register.jsx
+++ client/src/Pages/Authentication/Register.jsx
@@ -1,195 +1,102 @@
-import { useContext } from "react"
-import toast from "react-hot-toast"
-import { Link, useNavigate } from "react-router-dom"
-import { AuthContext } from "../../Provider/AuthProvider"
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../../Provider/AuthProvider";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from 'sweetalert2'
 
-const Registration = () => {
-  const navigate = useNavigate()
-  const { signInWithGoogle, createUser, updateUserProfile, user, setUser } = useContext(AuthContext)
+const Register = () => {
 
-  const handleSignUp = async e => {
-    e.preventDefault()
-    const form = e.target
-    const email = form.email.value
-    const name = form.name.value
-    const photo = form.photo.value
-    const pass = form.password.value
-    console.log({ email, pass, name, photo })
-    try {
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-      //User Registration
-      const result = await createUser(email, pass)
-      console.log(result)
-      await updateUserProfile(name, photo)
-      setUser({ ...user, photoURL: photo, displayName: name })
-      navigate('/')
-      toast.success('Signup Successful')
-    } catch (err) {
-      console.log(err)
-      toast.error(err?.message)
-    }
-  }
+  const onSubmit = data => {
+    console.log(data);
+    createUser(data.email, data.password)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        updateUserProfile(data.name, data.photoURL)
+          .then(() => {
+            console.log('user profile info updated')
+            reset();
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: 'User created successfully.',
+              showConfirmButton: false,
+              timer: 1500
+            });
+            navigate('/');
 
-  // Google 
-  const handleGoogleSignIn = async () => {
-    try {
-      await signInWithGoogle()
-      toast.success('Signin Successful')
-      navigate('/')
-    } catch (err) {
-      console.log(err)
-      toast.error(err?.message)
-    }
-  }
+          })
+          .catch(error => console.log(error))
+      })
+  };
+
   return (
-    <div className='flex justify-center items-center my-12 min-h-[calc(100vh-306px)]'>
-      <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
-        <div className='w-full px-6 py-8 md:px-8 lg:w-1/2'>
-          <div className='flex justify-center mx-auto'>
-            <img
-              className='w-auto h-7 sm:h-8'
-              src='../../../public/logo.png'
-              alt=''
-            />
+    <>
+      <Helmet>
+        <title>Bistro Boss | Sign Up</title>
+      </Helmet>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign up now!</h1>
+            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           </div>
-
-          <p className='mt-3 text-xl text-center text-gray-600 '>
-            Get Your Free Account Now.
-          </p>
-
-          <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
-            <div className='px-4 py-2'>
-              <svg className='w-6 h-6' viewBox='0 0 40 40'>
-                <path
-                  d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
-                  fill='#FFC107'
-                />
-                <path
-                  d='M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z'
-                  fill='#FF3D00'
-                />
-                <path
-                  d='M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z'
-                  fill='#4CAF50'
-                />
-                <path
-                  d='M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z'
-                  fill='#1976D2'
-                />
-              </svg>
-            </div>
-
-            <span className='w-5/6 px-4 py-3 font-bold text-center'>
-              Sign in with Google
-            </span>
-          </div>
-
-          <div className='flex items-center justify-between mt-4'>
-            <span className='w-1/5 border-b  lg:w-1/4'></span>
-
-            <div className='text-xs text-center text-gray-500 uppercase  hover:underline'>
-              or Registration with email
-            </div>
-
-            <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
-          </div>
-          <form onSubmit={handleSignUp}>
-            <div className='mt-4'>
-              <label
-                className='block mb-2 text-sm font-medium text-gray-600 '
-                htmlFor='name'
-              >
-                Username
-              </label>
-              <input
-                id='name'
-                autoComplete='name'
-                name='name'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
-                type='text'
-              />
-            </div>
-            <div className='mt-4'>
-              <label
-                className='block mb-2 text-sm font-medium text-gray-600 '
-                htmlFor='photo'
-              >
-                Photo URL
-              </label>
-              <input
-                id='photo'
-                autoComplete='photo'
-                name='photo'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
-                type='text'
-              />
-            </div>
-            <div className='mt-4'>
-              <label
-                className='block mb-2 text-sm font-medium text-gray-600 '
-                htmlFor='LoggingEmailAddress'
-              >
-                Email Address
-              </label>
-              <input
-                id='LoggingEmailAddress'
-                autoComplete='email'
-                name='email'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
-                type='email'
-              />
-            </div>
-
-            <div className='mt-4'>
-              <div className='flex justify-between'>
-                <label
-                  className='block mb-2 text-sm font-medium text-gray-600 '
-                  htmlFor='loggingPassword'
-                >
-                  Password
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+                {errors.name && <span className="text-red-600">Name is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                {errors.email && <span className="text-red-600">Email is required</span>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password"  {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                  pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                })} placeholder="password" className="input input-bordered" />
+                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
               </div>
-
-              <input
-                id='loggingPassword'
-                autoComplete='current-password'
-                name='password'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
-                type='password'
-              />
-            </div>
-            <div className='mt-6'>
-              <button
-                type='submit'
-                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-
-          <div className='flex items-center justify-between mt-4'>
-            <span className='w-1/5 border-b  md:w-1/4'></span>
-
-            <Link
-              to='/login'
-              className='text-xs text-gray-500 uppercase  hover:underline'
-            >
-              or sign in
-            </Link>
-
-            <span className='w-1/5 border-b  md:w-1/4'></span>
+              <div className="form-control mt-6">
+                <input className="btn btn-primary" type="submit" value="Sign Up" />
+              </div>
+            </form>
+            <p><small>Already have an account <Link to="/login">Login</Link></small></p>
           </div>
         </div>
-        <div
-          className='hidden bg-cover bg-center lg:block lg:w-1/2'
-          style={{
-            backgroundImage: `url(${"../../../public/register.jpg"})`,
-          }}
-        ></div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Registration
+export default Register;
