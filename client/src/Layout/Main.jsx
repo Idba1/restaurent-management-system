e76441b0ef6b29ae23fb7fbb67efeb6d444d329f
Idba1 <moniraislamself@gmail.com>
2025-03-08 02:12:45 +0600

    remove header and footer from login & signup page

--- client/src/Layout/Main.jsx
+++ client/src/Layout/Main.jsx
@@ -1,19 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar";
 import Footer from "../Pages/Shared/Footer";
 
 
 const Main = () => {
+    const location = useLocation();
+    // console.log(location)
+    const noHeaderFooter =  location.pathname.includes('login') || location.pathname.includes('signup');
     return (
         <div>
             {/* Navbar */}
-            <Navbar></Navbar>
+            {noHeaderFooter || <Navbar></Navbar>}
             {/* outlet */}
             <div className="min-h-[calc(100vh-306px)]">
                 <Outlet></Outlet>
             </div>
             {/* footer */}
-            <Footer></Footer>
+            {noHeaderFooter || <Footer></Footer>}
         </div>
     );
 };
