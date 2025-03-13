56e0f53c23a034462b699595490398b8e8361440
Idba1 <moniraislamself@gmail.com>
2025-03-13 13:14:38 +0600

    payment sysyem with stripe

--- client/src/Pages/Dashboard/Payment/Payment.jsx
+++ client/src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,17 +1,40 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
-const stripePromise = loadStripe('')
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+
 const Payment = () => {
+    const [isStripeLoaded, setIsStripeLoaded] = useState(false);
+
+    useEffect(() => {
+        stripePromise.then((stripe) => {
+            if (stripe) {
+                setIsStripeLoaded(true);
+                console.log("Stripe is loaded", stripe);
+            } else {
+                console.error("Stripe failed to load");
+            }
+        });
+
+        return () => {
+            setIsStripeLoaded(false);
+        };
+    }, []);
+
     return (
         <div>
             <SectionTitle heading="Payment" subHeading="Please pay to eat"></SectionTitle>
             <div>
-                <Elements stripe={stripePromise}>
-                    <CheckoutForm></CheckoutForm>
-                </Elements>
+                {isStripeLoaded ? (
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                ) : (
+                    <div>Loading Stripe...</div>
+                )}
             </div>
         </div>
     );
